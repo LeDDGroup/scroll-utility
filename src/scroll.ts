68490915d4fa727d65d761309d4fd98af86166b2
David Perez Alvarez <davidpa9708@gmail.com>
2018-03-23 19:07:26 -0400

    Add scrollToStart function

--- src/scroll.ts
+++ src/scroll.ts
@@ -3,6 +3,10 @@ export {
 };
 
 class Scroll {
+    public scrollToStart() {
+        const value = -window.scrollY;
+        window.scrollBy(0, value);
+    }
     public dist(value: number) {
         window.scrollBy(0, value);
     }
