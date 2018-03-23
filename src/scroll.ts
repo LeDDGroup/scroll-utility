f66f0d033983a09f395fb8a9fd98c46fdf967181
David Perez Alvarez <davidpa9708@gmail.com>
2018-03-23 19:29:58 -0400

    Allow scrolling inside elements

--- src/scroll.ts
+++ src/scroll.ts
@@ -3,23 +3,31 @@ export {
 };
 
 class Scroll {
+    private scrollable: HTMLElement;
+    constructor(scrollable?: HTMLElement) {
+        if (!!scrollable) {
+            this.scrollable = scrollable;
+        } else {
+            this.scrollable = document.body;
+        }
+    }
     public scrollToStart() {
-        const value = -window.scrollY;
-        window.scrollBy(0, value);
+        const value = this.getScrollPosition();
+        this.scrollable.scrollBy(0, -value);
     }
     public scrollToEnd() {
         const documentLength = this.getScrollHeight();
         const scrollPosition = this.getScrollPosition();
         const value = documentLength - scrollPosition;
-        window.scrollBy(0, value);
+        this.scrollable.scrollBy(0, value);
     }
     public dist(value: number) {
-        window.scrollBy(0, value);
+        this.scrollable.scrollBy(0, value);
     }
     private getScrollHeight(): number {
-        return document.body.scrollHeight;
+        return this.scrollable.scrollHeight;
     }
     private getScrollPosition(): number {
-        return window.scrollY;
+        return this.scrollable.scrollTop;
     }
 }
