2a46bf373cfca3e567c99d08420efed304631a83
David Perez Alvarez <davidpa9708@gmail.com>
2018-03-23 19:36:58 -0400

    Rename `dist` function to `scrollAmount`

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,15 @@
 import { Scroll } from "../src/scroll";
 
+
+//inner element
 const scrollable = document.getElementById("scrollable");
 const scroll = new Scroll(scrollable);
-
 scroll.scrollToEnd();
+const randomValue = (Math.random() - 0.5) * 200;
+scroll.scrollAmount(randomValue);
+
+//no element
+const windowScroll = new Scroll();
+windowScroll.scrollToEnd();
+const randomValue2 = (Math.random() - 0.5) * 200;
+windowScroll.scrollAmount(randomValue2);
