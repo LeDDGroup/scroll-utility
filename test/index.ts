85441c07a9af37f3774133d30508d8edff89abf1
David Perez Alvarez <davidpa9708@gmail.com>
2018-03-24 19:00:08 -0400

    Update tests

--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,11 @@
-import { Scroll } from "../src/scroll";
+import { Scroll, IProps } from "../src/scroll";
 
-
-//inner element
 const scrollable = document.getElementById("scrollable");
-const scrollTo = document.getElementById("element1");
-const scroll = new Scroll();
-const props = {
-    center: true,
-    value: 20,
-    smooth: true,
+const scrollElement = document.getElementById("element");
+const scroll = new Scroll(scrollable);
+const props: IProps = {
+    element: scrollElement,
+    percent: 100,
 }
 
-scroll.scrollToElement(scrollTo, props);
+scroll.scrollTo(props);
