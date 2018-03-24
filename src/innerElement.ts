804d9c73142fdb582c104411568aa39f89e6930a
David Perez Alvarez <david@leddgroup.com>
2018-03-23 21:41:37 -0400

    Add `innerElement`

---
+++ src/innerElement.ts
@@ -0,0 +1,37 @@
+export {
+    innerElement,
+}
+
+class innerElement {
+    private element: HTMLElement;
+    constructor(element: HTMLElement) {
+        this.element = element;
+    }
+    private get top() {
+        return this.rect.top;
+    }
+    private get left() {
+        return this.rect.left;
+    }
+    private get bottom() {
+        return this.rect.bottom;
+    }
+    private get right() {
+        return this.rect.right;
+    }
+    private get width() {
+        return this.right - this.left;
+    }
+    private get height() {
+        return this.bottom - this.top;
+    }
+    private get middleX() {
+        return (this.rect.left + this.rect.right) / 2;
+    }
+    private get middleY() {
+        return (this.rect.top + this.rect.bottom) / 2;
+    }
+    private get rect(): ClientRect {
+        return this.element.getBoundingClientRect();
+    }
+}
