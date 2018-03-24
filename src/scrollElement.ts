3a248943ea637a1d48602d33181e32f2c7edc900
David Perez Alvarez <david@leddgroup.com>
2018-03-23 23:22:09 -0400

    Refactor `smoothScroll` utilization

--- src/scrollElement.ts
+++ src/scrollElement.ts
@@ -1,3 +1,5 @@
+import { SmoothScroll, IProps as ISmoothProps } from "./smoothScroll";
+
 export {
     ScrollElement,
 }
@@ -5,14 +7,23 @@ export {
 class ScrollElement {
     private scrollable: HTMLElement;
     private isWindow: boolean;
+    private smoothScroll: SmoothScroll;
     constructor(element: HTMLElement) {
+        this.scrollBy = this.scrollBy.bind(this);
         if (!!element) {
             this.scrollable = element;
         } else {
             this.isWindow = true;
         }
+        const props: ISmoothProps = {
+            scrollBy: this.scrollBy,
+        };
+        this.smoothScroll = new SmoothScroll(props);
     }
-    public scroll(value: number) {
+    public scroll(value: number, smooth: boolean = false) {
+        this.smoothScroll.go(value);
+    }
+    private scrollBy(value: number) {
         const x = this.getX();
         const y = this.getY() + value;
         if (this.isWindow) {
