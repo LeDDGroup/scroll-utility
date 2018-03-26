88cee25b458e3e44cc72f64e0c8cd9e902127baa
David Perez Alvarez <david@leddgroup.com>
2018-03-26 16:32:38 -0400

    Add types to easing functions

--- src/easings.ts
+++ src/easings.ts
@@ -5,202 +5,201 @@ export {
 const easing = {
 
     // simple linear tweening - no easing, no acceleration
-
-    linearTween: function(t, b, c, d) {
-        return c * t / d + b;
+    linearTween: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        return distance * currentStep / totalSteps + offsetValue;
     },
 
 
     // quadratic easing in - accelerating from zero velocity
 
-    easeInQuad: function(t, b, c, d) {
-        t /= d;
-        return c * t * t + b;
+    easeInQuad: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        return distance * currentStep * currentStep + offsetValue;
     },
 
 
     // quadratic easing out - decelerating to zero velocity
 
-    easeOutQuad: function(t, b, c, d) {
-        t /= d;
-        return -c * t * (t - 2) + b;
+    easeOutQuad: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        return -distance * currentStep * (currentStep - 2) + offsetValue;
     },
 
 
 
     // quadratic easing in/out - acceleration until halfway, then deceleration
 
-    easeInOutQuad: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
+    easeInOutQuad: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps / 2;
+        if (currentStep < 1) return distance / 2 * currentStep * currentStep + offsetValue;
+        currentStep--;
+        return -distance / 2 * (currentStep * (currentStep - 2) - 1) + offsetValue;
     },
 
 
     // cubic easing in - accelerating from zero velocity
 
-    easeInCubic: function(t, b, c, d) {
-        t /= d;
-        return c * t * t * t + b;
+    easeInCubic: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        return distance * currentStep * currentStep * currentStep + offsetValue;
     },
 
 
 
     // cubic easing out - decelerating to zero velocity
 
-    easeOutCubic: function(t, b, c, d) {
-        t /= d;
-        t--;
-        return c * (t * t * t + 1) + b;
+    easeOutCubic: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        currentStep--;
+        return distance * (currentStep * currentStep * currentStep + 1) + offsetValue;
     },
 
 
 
     // cubic easing in/out - acceleration until halfway, then deceleration
 
-    easeInOutCubic: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t * t + b;
-        t -= 2;
-        return c / 2 * (t * t * t + 2) + b;
+    easeInOutCubic: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps / 2;
+        if (currentStep < 1) return distance / 2 * currentStep * currentStep * currentStep + offsetValue;
+        currentStep -= 2;
+        return distance / 2 * (currentStep * currentStep * currentStep + 2) + offsetValue;
     },
 
 
     // quartic easing in - accelerating from zero velocity
 
-    easeInQuart: function(t, b, c, d) {
-        t /= d;
-        return c * t * t * t * t + b;
+    easeInQuart: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        return distance * currentStep * currentStep * currentStep * currentStep + offsetValue;
     },
 
 
 
     // quartic easing out - decelerating to zero velocity
 
-    easeOutQuart: function(t, b, c, d) {
-        t /= d;
-        t--;
-        return -c * (t * t * t * t - 1) + b;
+    easeOutQuart: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        currentStep--;
+        return -distance * (currentStep * currentStep * currentStep * currentStep - 1) + offsetValue;
     },
 
 
 
     // quartic easing in/out - acceleration until halfway, then deceleration
 
-    easeInOutQuart: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t * t * t + b;
-        t -= 2;
-        return -c / 2 * (t * t * t * t - 2) + b;
+    easeInOutQuart: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps / 2;
+        if (currentStep < 1) return distance / 2 * currentStep * currentStep * currentStep * currentStep + offsetValue;
+        currentStep -= 2;
+        return -distance / 2 * (currentStep * currentStep * currentStep * currentStep - 2) + offsetValue;
     },
 
 
     // quintic easing in - accelerating from zero velocity
 
-    easeInQuint: function(t, b, c, d) {
-        t /= d;
-        return c * t * t * t * t * t + b;
+    easeInQuint: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        return distance * currentStep * currentStep * currentStep * currentStep * currentStep + offsetValue;
     },
 
 
 
     // quintic easing out - decelerating to zero velocity
 
-    easeOutQuint: function(t, b, c, d) {
-        t /= d;
-        t--;
-        return c * (t * t * t * t * t + 1) + b;
+    easeOutQuint: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        currentStep--;
+        return distance * (currentStep * currentStep * currentStep * currentStep * currentStep + 1) + offsetValue;
     },
 
 
 
     // quintic easing in/out - acceleration until halfway, then deceleration
 
-    easeInOutQuint: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t * t * t * t + b;
-        t -= 2;
-        return c / 2 * (t * t * t * t * t + 2) + b;
+    easeInOutQuint: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps / 2;
+        if (currentStep < 1) return distance / 2 * currentStep * currentStep * currentStep * currentStep * currentStep + offsetValue;
+        currentStep -= 2;
+        return distance / 2 * (currentStep * currentStep * currentStep * currentStep * currentStep + 2) + offsetValue;
     },
 
 
     // sinusoidal easing in - accelerating from zero velocity
 
-    easeInSine: function(t, b, c, d) {
-        return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
+    easeInSine: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        return -distance * Math.cos(currentStep / totalSteps * (Math.PI / 2)) + distance + offsetValue;
     },
 
 
 
     // sinusoidal easing out - decelerating to zero velocity
 
-    easeOutSine: function(t, b, c, d) {
-        return c * Math.sin(t / d * (Math.PI / 2)) + b;
+    easeOutSine: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        return distance * Math.sin(currentStep / totalSteps * (Math.PI / 2)) + offsetValue;
     },
 
 
 
     // sinusoidal easing in/out - accelerating until halfway, then decelerating
 
-    easeInOutSine: function(t, b, c, d) {
-        return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+    easeInOutSine: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        return -distance / 2 * (Math.cos(Math.PI * currentStep / totalSteps) - 1) + offsetValue;
     },
 
 
 
     // exponential easing in - accelerating from zero velocity
 
-    easeInExpo: function(t, b, c, d) {
-        return c * Math.pow(2, 10 * (t / d - 1)) + b;
+    easeInExpo: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        return distance * Math.pow(2, 10 * (currentStep / totalSteps - 1)) + offsetValue;
     },
 
 
 
     // exponential easing out - decelerating to zero velocity
 
-    easeOutExpo: function(t, b, c, d) {
-        return c * (-Math.pow(2, -10 * t / d) + 1) + b;
+    easeOutExpo: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        return distance * (-Math.pow(2, -10 * currentStep / totalSteps) + 1) + offsetValue;
     },
 
 
 
     // exponential easing in/out - accelerating until halfway, then decelerating
 
-    easeInOutExpo: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-        t--;
-        return c / 2 * (-Math.pow(2, -10 * t) + 2) + b;
+    easeInOutExpo: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps / 2;
+        if (currentStep < 1) return distance / 2 * Math.pow(2, 10 * (currentStep - 1)) + offsetValue;
+        currentStep--;
+        return distance / 2 * (-Math.pow(2, -10 * currentStep) + 2) + offsetValue;
     },
 
 
     // circular easing in - accelerating from zero velocity
 
-    easeInCirc: function(t, b, c, d) {
-        t /= d;
-        return -c * (Math.sqrt(1 - t * t) - 1) + b;
+    easeInCirc: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        return -distance * (Math.sqrt(1 - currentStep * currentStep) - 1) + offsetValue;
     },
 
 
 
     // circular easing out - decelerating to zero velocity
 
-    easeOutCirc: function(t, b, c, d) {
-        t /= d;
-        t--;
-        return c * Math.sqrt(1 - t * t) + b;
+    easeOutCirc: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps;
+        currentStep--;
+        return distance * Math.sqrt(1 - currentStep * currentStep) + offsetValue;
     },
 
 
 
     // circular easing in/out - acceleration until halfway, then deceleration
 
-    easeInOutCirc: function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
-        t -= 2;
-        return c / 2 * (Math.sqrt(1 - t * t) + 1) + b;
+    easeInOutCirc: function(currentStep: number, offsetValue: number, distance: number, totalSteps: number) {
+        currentStep /= totalSteps / 2;
+        if (currentStep < 1) return -distance / 2 * (Math.sqrt(1 - currentStep * currentStep) - 1) + offsetValue;
+        currentStep -= 2;
+        return distance / 2 * (Math.sqrt(1 - currentStep * currentStep) + 1) + offsetValue;
     },
 }
 
