2655bde4c18ee7b6c8cce84d631cbc7318a861a4
David Perez Alvarez <david@leddgroup.com>
2018-03-28 13:08:35 -0400

    Basic testing mocha parcel zombie local

--- test/index.ts
+++ test/index.ts
@@ -1,18 +1,23 @@
 import { Scroll, IProps } from "../src/scroll";
+import { getBody } from "./body";
 
-const scrollable = document.getElementById("scrollable");
-const scrollElement = document.getElementById("element");
-const scroll = new Scroll(scrollable);
-const props: IProps = {
-    element: scrollElement,
-    percent: 50,
-    duration: 1000,
-}
 
-const windowScroll = new Scroll();
-scroll.scrollTo(props);
-windowScroll.scrollTo({
-    element: scrollable,
-    percent: 50,
-    duration: 1000,
-});
+window.onload = () => {
+    document.body.innerHTML = getBody();
+    const scrollable = document.getElementById("scrollable");
+    const scrollElement = document.getElementById("element");
+    const scroll = new Scroll(scrollable);
+    const props: IProps = {
+        element: scrollElement,
+        percent: 50,
+        duration: 1000,
+    }
+
+    const windowScroll = new Scroll();
+    scroll.scrollTo(props);
+    windowScroll.scrollTo({
+        element: scrollable,
+        percent: 50,
+        duration: 1000,
+    });
+};
