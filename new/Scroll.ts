7a95a526d7f46d2ee90aa5a6858f6f62b3bd33c7
David Perez Alvarez <david@leddgroup.com>
2018-07-04 11:24:07 -0400

    Add getter functions

--- new/Scroll.ts
+++ new/Scroll.ts
@@ -12,15 +12,29 @@ interface IOptions {
 
 class Scroll {
   private scrollElement: any = {};
-  constructor(private element?: HTMLElement, options?: IOptions) {
+  private isWindow = false;
+  constructor(private element?: HTMLElement) {
+    this.isWindow = element === undefined || element === null;
   }
-  public scrollToElement(element: HTMLElement): ScrollInstance {
+  public scrollToElement(element: HTMLElement, options?: IOptions): ScrollInstance {
     return new ScrollInstance;
   }
-  public scrollToPercent(percent: number): ScrollInstance {
+  public scrollToPercent(percent: number, options?: IOptions): ScrollInstance {
     return new ScrollInstance;
   }
   public doScroll(options: IOptions): ScrollInstance {
     return new ScrollInstance;
+  }
+  private get scrollHeight(): number {
+    return this.isWindow ? document.body.clientHeight : this.element.scrollHeight;
+  }
+  private get scrollWidth(): number {
+    return this.isWindow ? document.body.clientWidth : this.element.scrollWidth;
+  }
+  private get height(): number {
+    return this.isWindow ? window.innerHeight : this.element.clientHeight;
+  }
+  private get width(): number {
+    return this.isWindow ? window.innerWidth : this.element.clientWidth;
   }
 }
