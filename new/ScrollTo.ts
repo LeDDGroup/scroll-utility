71abf45b1fce38294de45871ab8e1797a0be44ce
David Perez Alvarez <david@leddgroup.com>
2018-07-04 12:38:23 -0400

    Basic working scroll

--- new/ScrollTo.ts
+++ new/ScrollTo.ts
@@ -6,6 +6,7 @@ export {
 }
 
 interface ScrollInstanceProps {
+  element?: HTMLElement;
   duration: number;
   distToScroll: () => number;
 }
@@ -14,19 +15,31 @@ type DOMHighResTimeStamp = number;
 
 class ScrollInstance {
   private initialTime: DOMHighResTimeStamp;
+  private element: HTMLElement;
+  private isWindow: boolean;
+  private initialPosition: number;
   constructor(private options: ScrollInstanceProps) {
+    this.element = options.element;
+    this.isWindow = !options.element;
+    this.scroll = this.scroll.bind(this)
     this.scroll = this.scroll.bind(this)
     this.initialTime = performance.now();
-    window.requestAnimationFrame(scroll);
+    this.initialPosition = this.isWindow ? window.pageYOffset : this.element.scrollTop;
+    window.requestAnimationFrame(this.scroll);
   }
   private scroll() {
     const currentTime = performance.now();
     const currentDuration = currentTime - this.initialTime;
-    if (currentDuration >= this.options.duration) {
-      easing.linear(this.options.duration, 0, this.options.distToScroll(), this.options.duration);
+    if (currentDuration < this.options.duration) {
+      const distToScroll = easing.linear(currentDuration, 0, this.options.distToScroll(), this.options.duration)
+      this.doScroll(distToScroll);
+      window.requestAnimationFrame(this.scroll);
     } else {
-      easing.linear(currentDuration, 0, this.options.distToScroll(), this.options.duration);
-      window.requestAnimationFrame(scroll);
+      const distToScroll = easing.linear(this.options.duration, 0, this.options.distToScroll(), this.options.duration)
+      this.doScroll(distToScroll);
     }
+  }
+  private doScroll(distance: number) {
+    this.isWindow ? window.scroll(0, distance + this.initialPosition) : this.element.scroll(0, distance + this.initialPosition);
   }
 }
