813eeed53cbb4a7451551bac232843fd4f5e8ad2
David Perez Alvarez <david@leddgroup.com>
2018-07-06 19:49:11 -0400

    Changing exports

--- new/Scroll.ts
+++ new/Scroll.ts
@@ -1,94 +1,117 @@
 import { Animation as ScrollAnimation } from "./Animation";
 
-export { Scroll, IOptions };
-
-interface IOptions {
-  offset?: number;
-  duration?: number;
-}
+export { Scroll };
 
 class Scroll {
-  private animations: ScrollAnimation[] = [];
+  private verticalScrollAnimation: ScrollAnimation[] = [];
+  private horizontalScrollAnimation: ScrollAnimation[] = [];
   private scrollChanged: number = 0;
+  private animations: number = 0;
   constructor(private element?: HTMLElement) {
     this.onAnimationFrame = this.onAnimationFrame.bind(this);
   }
-  public scrollToElement(element: HTMLElement, options?: IOptions) {
-    const offset = options.offset || 0;
-    const distToElement = element.getBoundingClientRect().top - this.top + offset;
+  public verticalScroll() {
+    return {
+      toElement: () => null,
+      toPercent: () => null,
+      do: () => null,
+    }
+  }
+  public horizontalScroll() {
+    return {
+      toElement: () => null,
+      toPercent: () => null,
+      do: () => null,
+    }
+  }
+  public scroll() {
+    return {
+      toElement: () => null,
+      toPercent: () => null,
+      do: () => null,
+    }
+  }
+  private scrollToElement(element: HTMLElement, horizontal: boolean, duration: number) {
+    const distToElement = this.getBounding(horizontal) - this.offset(horizontal);
     this.createScrollAnimation({
       distToScroll: () => distToElement,
-      duration: options.duration,
+      duration,
+      horizontal,
     });
   }
-  public scrollToPercent(percent: number = 0, options?: IOptions) {
-    const offset = options.offset || 0;
-    const dist = ((this.scrollHeight - this.height) * percent) / 100 - this.y + offset;
+  private scrollToPercent(percent: number = 0, horizontal: boolean, duration: number) {
+    const dist = ((this.scrollSize(horizontal) - this.size(horizontal)) * percent) / 100 - this.position(horizontal);
     this.createScrollAnimation({
       distToScroll: () => dist,
-      duration: options.duration,
+      duration,
+      horizontal,
     });
   }
-  public doScroll(options: IOptions) {
+  private doScroll(amount, horizontal, duration) {
     this.createScrollAnimation({
-      distToScroll: () => options.offset,
-      duration: options.duration,
+      distToScroll: () => amount,
+      duration,
+      horizontal,
     });
-  }
-  private get scrollHeight(): number {
-    return this.isWindow ? document.body.clientHeight : this.element.scrollHeight;
-  }
-  private get scrollWidth(): number {
-    return this.isWindow ? document.body.clientWidth : this.element.scrollWidth;
-  }
-  private get height(): number {
-    return this.isWindow ? window.innerHeight : this.element.clientHeight;
-  }
-  private get width(): number {
-    return this.isWindow ? window.innerWidth : this.element.clientWidth;
-  }
-  private get y(): number {
-    return this.isWindow ? window.pageYOffset : this.element.scrollTop;
-  }
-  private get x(): number {
-    return this.isWindow ? window.pageXOffset : this.element.scrollLeft;
-  }
-  private get top(): number {
-    return this.isWindow ? 0 : this.element.getBoundingClientRect().top;
-  }
-  private get left(): number {
-    return this.isWindow ? 0 : this.element.getBoundingClientRect().left;
-  }
-  private createScrollAnimation(options: {
-    distToScroll: () => number;
-    duration: number;
-  }): ScrollAnimation {
-    const firstAnimation = this.animations.every(() => false);
-    const index = this.animations.length;
-    const animation = new ScrollAnimation({
-      distToScroll: options.distToScroll,
-      duration: options.duration,
-      stop: () => {
-        this.scrollChanged -= this.animations[index].distance;
-        delete this.animations[index];
-      },
-    });
-    this.animations.push(animation);
-    if (firstAnimation) {
-      window.requestAnimationFrame(this.onAnimationFrame);
-    }
-    return animation;
   }
   private get isWindow(): boolean {
     return !this.element;
   }
+  private getBounding(horizontal: boolean) {
+    return horizontal ?
+      this.isWindow ? 0 : element.getBoundingClientRect().left:
+    this.isWindow ? 0 : element.getBoundingClientRect().top;
+  }
+  private scrollSize(horizontal: boolean) {
+    return horizontal ?
+      this.isWindow ? document.body.clientWidth : this.element.scrollWidth:
+    this.isWindow ? document.body.clientHeight : this.element.scrollHeight;
+  }
+  private size(horizontal: boolean) {
+    return horizontal ?
+      this.isWindow ? window.innerWidth : this.element.clientWidth:
+    this.isWindow ? window.innerHeight : this.element.clientHeight;
+  }
+  private position(horizontal: boolean) {
+    return horizontal ?
+      this.isWindow ? window.pageXOffset : this.element.scrollLeft:
+    this.isWindow ? window.pageYOffset : this.element.scrollTop;
+  }
+  private offset(horizontal: boolean) {
+    return horizontal ?
+      this.isWindow ? 0 : this.element.getBoundingClientRect().left
+    :
+    this.isWindow ? 0 : this.element.getBoundingClientRect().top;
+  }
+  private createScrollAnimation(options: {
+    distToScroll: () => number;
+    duration: number;
+    horizontal: boolean;
+  }): ScrollAnimation {
+    const container = options.horizontal ? this.horizontalScrollAnimation : this.verticalScrollAnimation;
+    const index = container.length;
+    const animation = new ScrollAnimation({
+      distToScroll: options.distToScroll,
+      duration: options.duration,
+      stop: () => {
+        this.scrollChanged -= container[index].distance;
+        this.animations--;
+        delete container[index];
+      },
+    });
+    container.push(animation);
+    if (this.animations === 1) {
+      window.requestAnimationFrame(this.onAnimationFrame);
+    }
+    return animation;
+  }
   private onAnimationFrame() {
-    let distToScroll = 0;
-    const scrollPosition = this.y;
-    const initial = this.y - this.scrollChanged;
-    if (this.animations.every(() => false)) {
+    if (this.animations === 0) {
       this.scrollChanged = 0;
     } else {
+      let distToScroll = 0;
+      const scrollPosition = this.y;
+      const initial = this.y - this.scrollChanged;
       this.animations.forEach((animation, index) => {
         distToScroll += animation.distance;
       });
