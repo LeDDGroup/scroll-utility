3e609e9c2fee6c19c09a987c919fb0026243fc00
David Perez Alvarez <david@leddgroup.com>
2018-07-06 22:57:05 -0400

    Update tests

--- test/local/index.ts
+++ test/local/index.ts
@@ -5,15 +5,22 @@ export = Scroll;
 const windowScroll = new Scroll();
 
 window.onkeydown = (ev) => {
-    if (ev.key === "ArrowUp" || ev.key === "ArrowDown") {
-        ev.preventDefault();
-    }
+  if (ev.key === "ArrowUp" || ev.key === "ArrowDown") {
+    ev.preventDefault();
+  }
 };
 window.onkeyup = (ev) => {
-    if (ev.key === "ArrowUp") {
-      windowScroll.verticalScroll().do(-500, 1000);
-    }
-    if (ev.key === "ArrowDown") {
-      windowScroll.verticalScroll().do(500, 1000);
-    }
+  if (ev.key === "ArrowUp") {
+    windowScroll.verticalScroll.do(-500, 1000);
+  }
+  if (ev.key === "ArrowDown") {
+    windowScroll.verticalScroll.do(500, 1000);
+  }
 }
+
+const scrollable = document.getElementById("scrollable");
+const element = document.getElementById("element");
+const element1 = document.getElementById("element1");
+windowScroll.scroll.toElement(element1, 1000)
+const elementScroll = new Scroll(scrollable);
+elementScroll.scroll.toElement(element, 1000);
