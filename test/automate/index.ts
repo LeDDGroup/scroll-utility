b22e67e6ca7fdd1057dbca013e64c2a84cc7144d
David Perez Alvarez <david@leddgroup.com>
2018-07-10 12:25:30 -0400

    Basic working tests

--- test/automate/index.ts
+++ test/automate/index.ts
@@ -1,23 +1,29 @@
 import capabilities from "./capabilities";
 import * as webdriver from "selenium-webdriver";
+import { expect } from "chai";
 
 const cap = capabilities.window.chrome;
 
-let browser: any;
+let browser: webdriver.WebDriver = (null as any) as webdriver.WebDriver;
 
-before(async () => {
+before(async function() {
+  this.timeout(10000);
   browser = await new webdriver.Builder()
     .usingServer("http://hub-cloud.browserstack.com/wd/hub")
     .withCapabilities(cap)
     .build();
 });
 
-describe("client", async () => {
-  let browser: webdriver.WebDriver = (null as any) as webdriver.WebDriver;
-  describe("first test", () => {
+describe("client", async function() {
+  this.timeout(20000);
+    describe("first test", () => {
+    it("should do something", () => {
+      expect("asdf").to.be.eq("asdf");
+    })
   });
 });
 
-after(async () => {
+after(async function() {
+  this.timeout(10000);
   await browser.quit();
 });
