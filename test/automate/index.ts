3d51e96e1a7661fe6c4f7ef3cef89388b87c874b
David Perez Alvarez <david@leddgroup.com>
2018-07-30 03:06:34 -0400

    Fix up some values

--- test/automate/index.ts
+++ test/automate/index.ts
@@ -8,21 +8,31 @@ import { testScenarios } from "./scenaries";
 const local_testing_site_url = "http://localhost:8080/";
 const long_timeout = 0;
 
-for (const os in capabilities) {
-  for (const browser in capabilities[os]) {
-    test(os, browser);
+let server = new Server();
+before(async function() {
+  this.timeout(long_timeout);
+  await server.start();
+});
+describe("client tests ", async function() {
+  this.timeout(long_timeout);
+  for (const os in capabilities) {
+    for (const browser in capabilities[os]) {
+      test(os, browser);
+    }
   }
-}
+});
+after(async function() {
+  this.timeout(long_timeout);
+  await server.stop();
+});
 
 function test(os: string, browserId: string) {
   describe(`testing in ${os} ${browserId}`, async function() {
     this.timeout(long_timeout);
     const cap = capabilities[os][browserId];
     let browser: webdriver.WebDriver = (null as any) as webdriver.WebDriver;
-    let server = new Server();
     before(async function() {
       this.timeout(long_timeout);
-      await server.start();
       browser = await new webdriver.Builder()
         .usingServer("https://hub-cloud.browserstack.com/wd/hub")
         .withCapabilities(cap)
@@ -40,7 +50,6 @@ function test(os: string, browserId: string) {
     })
     after(async function() {
       this.timeout(long_timeout);
-      server.stop();
       await browser.quit();
     });
   });
