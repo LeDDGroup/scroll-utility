a1dd1add4cfd43e42d69edf311e141aab83fa259
David Perez Alvarez <david@leddgroup.com>
2018-10-25 03:23:59 -0400

    refactor: clean ScrollElement

--- src/element.ts
+++ src/element.ts
@@ -1,59 +1,71 @@
 import { onScroll } from "./scroll"
-// https://stackoverflow.com/questions/3437786/get-the-size-of-the-screen-current-web-page-and-browser-window
 
-const ADD_EVENT_LISTENER = "addEventListener"
-const REMOVE_EVENT_LISTENER = "removeEventListener"
+const body = document.body
+const html = document.documentElement || {
+  clientWidth: 0,
+  clientHeight: 0,
+  scrollWidth: 0,
+  scrollHeight: 0,
+  offsetWidth: 0,
+  offsetHeight: 0,
+}
+
+function matchHorizontal(h: () => number, v: () => number) {
+  return (horizontal: boolean) => {
+    return horizontal ? h() : v()
+  }
+}
+
+function windowSize() {
+  // https://stackoverflow.com/questions/3437786/get-the-size-of-the-screen-current-web-page-and-browser-window
+  return matchHorizontal(
+    () => html.clientWidth || body.clientWidth || window.innerWidth,
+    () => html.clientHeight || body.clientHeight || window.innerHeight,
+  )
+}
+
+function windowScrollSize() {
+  return matchHorizontal(
+    () =>
+      Math.max(
+        body.scrollWidth,
+        body.offsetWidth,
+        html.clientWidth,
+        html.scrollWidth,
+        html.offsetWidth,
+      ),
+    () =>
+      Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight,
+      ),
+  )
+}
 
 class ScrollElement {
-  constructor(private element?: HTMLElement | null) {
-    const body = document.body
-    const html = document.documentElement
-    if (html === null) {
-      throw new Error("document.documentElement is null") // TODO add fallback for documentElement being null
-    }
-
-    if (!element) {
-      this.size = (horizontal: boolean) =>
-        horizontal
-          ? html.clientWidth || body.clientWidth || window.innerWidth
-          : html.clientHeight || body.clientHeight || window.innerHeight
-
-      this.scrollSize = (horizontal: boolean) =>
-        horizontal
-          ? Math.max(
-              body.scrollWidth,
-              body.offsetWidth,
-              html.clientWidth,
-              html.scrollWidth,
-              html.offsetWidth,
-            )
-          : Math.max(
-              body.scrollHeight,
-              body.offsetHeight,
-              html.clientHeight,
-              html.scrollHeight,
-              html.offsetHeight,
-            )
-
-      this.position = (horizontal: boolean) =>
-        horizontal ? window.pageXOffset : window.pageYOffset
-
+  static isWindow(element: HTMLElement | Window): element is Window {
+    return element === window
+  }
+  constructor(private element: HTMLElement | Window = window) {
+    if (ScrollElement.isWindow(element)) {
+      this.size = windowSize()
+      this.scrollSize = windowScrollSize()
+      this.position = matchHorizontal(() => element.pageXOffset, () => element.pageYOffset)
       this.offset = () => 0
-
       this.scrollTo = (x: number, y: number) => {
-        window.scroll(x, y)
+        element.scroll(x, y)
       }
     } else {
-      this.size = (horizontal: boolean) => (horizontal ? element.clientWidth : element.clientHeight)
-
-      this.scrollSize = (horizontal: boolean) =>
-        horizontal ? element.scrollWidth : element.scrollHeight
-
-      this.position = (horizontal: boolean) => (horizontal ? element.scrollLeft : element.scrollTop)
-
-      this.offset = (horizontal: boolean) =>
-        horizontal ? element.getBoundingClientRect().left : element.getBoundingClientRect().top
-
+      this.size = matchHorizontal(() => element.clientWidth, () => element.clientHeight)
+      this.scrollSize = matchHorizontal(() => element.scrollWidth, () => element.scrollHeight)
+      this.position = matchHorizontal(() => element.scrollLeft, () => element.scrollTop)
+      this.offset = matchHorizontal(
+        () => element.getBoundingClientRect().left,
+        () => element.getBoundingClientRect().top,
+      )
       this.scrollTo = (x: number, y: number) => {
         element.scrollLeft = x
         element.scrollTop = y
@@ -77,8 +89,9 @@ class ScrollElement {
   public offset: (horizontal: boolean) => number
   public scrollTo: (x: number, y: number) => void
   private toggleMount(add: boolean) {
-    const f = add ? ADD_EVENT_LISTENER : REMOVE_EVENT_LISTENER
-    ;(!!this.element ? this.element[f] : window[f])("scroll", this.scroll)
+    add
+      ? this.element.addEventListener("scroll", this.scroll)
+      : this.element.removeEventListener("scroll", this.scroll)
   }
 }
 
