6d7c8b42399cb2764ae88ea64d83a9e7bf828ec3
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-10-25 00:51:16 -0400

    docs(demo): don't use document.body as application root

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,4 +2,8 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { App } from "./components/App"
 
-ReactDOM.render(<App />, document.body)
+const el = document.createElement("div")
+
+document.body.appendChild(el)
+
+ReactDOM.render(<App />, el)
