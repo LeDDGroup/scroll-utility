919bf92c13618f91e7fdf01407f26c40425f0eb1
David Perez Alvarez <david@leddgroup.com>
2019-04-30 05:37:54 -0400

    refactor: remove unnecessary getters

--- src/scroll.ts
+++ src/scroll.ts
@@ -11,10 +11,7 @@ type ScrollOptions = [number?, EasingFunction?]
 
 type ScrollType = ((value: number, ...args: ScrollOptions) => Scroll) & {
   size: (value: number, ...args: ScrollOptions) => Scroll
-  sizeWithBorders: (value: number, ...args: ScrollOptions) => Scroll
   scrollSize: (value: number, ...args: ScrollOptions) => Scroll
-  scrollPosition: (value: number, ...args: ScrollOptions) => Scroll
-  offset: (value: number, ...args: ScrollOptions) => Scroll
   element: (elementOrQuery: ElementOrQuery, value?: number, ...args: ScrollOptions) => Scroll
 }
 
@@ -62,20 +59,16 @@ class Scroll {
   get size() {
     return ScrollElement.getSize(this.element, this.horizontal)
   }
-  get sizeWithBorders() {
-    return ScrollElement.getSizeWithBorders(this.element, this.horizontal)
-  }
   get scrollSize() {
     return ScrollElement.getScrollSize(this.element, this.horizontal)
   }
   get scrollPosition() {
     return ScrollElement.getScrollPosition(this.element, this.horizontal)
   }
-  get offset() {
-    return ScrollElement.getOffset(this.element, this.horizontal)
-  }
   getElementPlacement(elementOrQuery: ElementOrQuery) {
-    const elementPosition = ScrollElement.getOffset(elementOrQuery, this.horizontal) - this.offset
+    const elementPosition =
+      ScrollElement.getOffset(elementOrQuery, this.horizontal) -
+      ScrollElement.getOffset(this.element, this.horizontal)
     const elementSize = ScrollElement.getSizeWithBorders(elementOrQuery, this.horizontal)
     const ratio = elementPosition / (this.size - elementSize)
     return ratio <= 1 && ratio >= 0
@@ -92,17 +85,12 @@ class Scroll {
     },
     {
       size: (value: number, ...args: ScrollOptions) => this.scrollTo(this.size * value, ...args),
-      sizeWithBorders: (value: number, ...args: ScrollOptions) =>
-        this.scrollTo(this.sizeWithBorders * value, ...args),
       scrollSize: (value: number, ...args: ScrollOptions) =>
         this.scrollTo(this.scrollSize * value, ...args),
-      scrollPosition: (value: number, ...args: ScrollOptions) =>
-        this.scrollTo(this.scrollPosition * value, ...args),
-      offset: (value: number, ...args: ScrollOptions) =>
-        this.scrollTo(this.offset * value, ...args),
       element: (elementOrQuery: ElementOrQuery, value: number = 0, ...args: ScrollOptions) => {
         const elementPosition =
-          ScrollElement.getOffset(elementOrQuery, this.horizontal) - this.offset
+          ScrollElement.getOffset(elementOrQuery, this.horizontal) -
+          ScrollElement.getOffset(this.element, this.horizontal)
         const elementSize = ScrollElement.getSizeWithBorders(elementOrQuery, this.horizontal)
 
         return this.scrollBy(
@@ -130,14 +118,8 @@ class Scroll {
     },
     {
       size: (value: number, ...args: ScrollOptions) => this.scrollBy(this.size * value, ...args),
-      sizeWithBorders: (value: number, ...args: ScrollOptions) =>
-        this.scrollBy(this.sizeWithBorders * value, ...args),
       scrollSize: (value: number, ...args: ScrollOptions) =>
         this.scrollBy(this.scrollSize * value, ...args),
-      scrollPosition: (value: number, ...args: ScrollOptions) =>
-        this.scrollBy(this.scrollPosition * value, ...args),
-      offset: (value: number, ...args: ScrollOptions) =>
-        this.scrollBy(this.offset * value, ...args),
       element: (elementOrQuery: ElementOrQuery, value: number = 0, ...args: ScrollOptions) => {
         return this.scrollBy(
           ScrollElement.getSizeWithBorders(elementOrQuery, this.horizontal) * value,
