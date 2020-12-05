894f473323cda4b056d2681f739de1ceb2c62ab3
David Perez Alvarez <david@leddgroup.com>
2020-12-05 15:43:37 -0500

    wip

--- src/misc.ts
+++ src/misc.ts
@@ -1,16 +1,19 @@
+type ScrollElement = Element | Window;
+
 export const getWidth = (el: ScrollElement) =>
 	isWindow(el)
 		? document.documentElement.clientWidth ||
 		  document.body.clientWidth ||
 		  window.innerWidth
 		: el.clientWidth;
-
 export const getHeight = (el: ScrollElement) =>
 	isWindow(el)
 		? document.documentElement.clientHeight ||
 		  document.body.clientHeight ||
 		  window.innerHeight
 		: el.clientHeight;
+export const getSize = (el: ScrollElement, horizontal) =>
+	horizontal ? getWidth(el) : getHeight(el);
 
 export const getRealScrollHeight = (el: ScrollElement) =>
 	isWindow(el)
@@ -22,7 +25,6 @@ export const getRealScrollHeight = (el: ScrollElement) =>
 				document.documentElement.offsetHeight
 		  )
 		: el.scrollHeight;
-
 export const getRealScrollWidth = (el: ScrollElement) =>
 	isWindow(el)
 		? Math.max(
@@ -33,16 +35,22 @@ export const getRealScrollWidth = (el: ScrollElement) =>
 				document.documentElement.offsetWidth
 		  )
 		: el.scrollWidth;
+export const getRealScrollSize = (el: ScrollElement, horizontal) =>
+	horizontal ? getRealScrollWidth(el) : getRealScrollHeight(el);
 
 export const getScrollWidth = (el: ScrollElement) =>
 	getScrollWidth(el) - getWidth(el);
 export const getScrollHeight = (el: ScrollElement) =>
 	getScrollHeight(el) - getHeight(el);
+export const getScrollSize = (el: ScrollElement, horizontal) =>
+	horizontal ? getScrollWidth(el) : getScrollHeight(el);
 
-export const getScrollWidth = (el: ScrollElement) =>
+export const getScrollTop = (el: ScrollElement) =>
 	isWindow(el) ? window.pageXOffset : el.scrollLeft;
-export const getScrollHeight = (el: ScrollElement) =>
+export const getScrollLeft = (el: ScrollElement) =>
 	isWindow(el) ? window.pageYOffset : el.scrollTop;
+export const getScrollPosition = (el: ScrollElement, horizontal) =>
+	horizontal ? getScrollLeft(el) : getScrollTop(el);
 
 export const getOffsetWidth = (el: ScrollElement) =>
 	isWindow(el)
@@ -56,6 +64,8 @@ export const getOffsetHeight = (el: ScrollElement) =>
 		  document.body.clientHeight ||
 		  window.innerHeight
 		: el.offsetHeight;
+export const getOffsetSize = (el: ScrollElement, horizontal) =>
+	horizontal ? getOffsetWidth(el) : getOffsetHeight(el);
 
 export const getBorderWidth = (el: ScrollElement) =>
 	isWindow(el)
@@ -79,29 +89,59 @@ export const getBorderHeight = (el: ScrollElement) =>
 				getComputedStyle(el, null).getPropertyValue("border-bottom-width"),
 				10
 		  ) || 0);
-export const getLeftRelativePosition = (el: ScrollElement) =>
+export const getBorder = (el: ScrollElement, horizontal) =>
+	horizontal ? getBorderWidth(el) : getBorderHeight(el);
+
+export const getLeftPosition = (el: ScrollElement) =>
 	isWindow(el) ? 0 : el.getBoundingClientRect().left;
-export const getTopRelativePosition = (el: ScrollElement) =>
+export const getTopPosition = (el: ScrollElement) =>
 	isWindow(el) ? 0 : el.getBoundingClientRect().top;
+export const getPosition = (el: ScrollElement, horizontal) =>
+	horizontal ? getLeftPosition(el) : getTopPosition(el);
 
 export const scrollTop = (el, value) => el.scrollBy(value, 0);
 export const scrollLeft = (el, value) => el.scrollBy(0, value);
+export const scroll = (el, value, horizontal) =>
+	horizontal ? scrollLeft(value) : scrollTop(value);
 
-export {
-	getWidth,
-	getHeight,
-	getRealScrollHeight,
-	getRealScrollWidth,
-	getScrollWidth,
-	getScrollHeight,
-	getScrollWidth,
-	getScrollHeight,
-	getOffsetWidth,
-	getOffsetHeight,
-	getBorderWidth,
-	getBorderHeight,
-	getLeftRelativePosition,
-	getTopRelativePosition,
-	scrollTop,
-	scrollLeft,
-};
+export const getOffset = (
+	container: ScrollElement,
+	el: ScrollElement,
+	horizontal: boolean
+) =>
+	getPosition(el, horizontal) -
+	getPosition(container, horizontal) -
+	getBorder(container, horizontal);
+
+function getDiff(
+	container: ScrollElement,
+	el: ScrollElement,
+	horizontal: boolean
+) {
+	return getSize(container, horizontal) - getPosition(el, horizontal);
+}
+
+export function getRelativePosition(
+	container: ScrollElement,
+	element: ScrollElement,
+	horizontal: boolean
+): number {
+	if (element === myContainer.element) {
+		return getScrollPosition() / getScrollSize();
+	}
+	return (
+		getOffset(container, element, horizontal) /
+		getDiff(container, element, horizontal)
+	);
+}
+
+export function getDistToElement(
+	container: ScrollElement,
+	element: ScrollElement,
+	horizontal
+): number {
+	return (
+		getOffset(container, element, horizontal) -
+		getDiff(container, element, horizontal)
+	);
+}
--- src/scroll.ts
+++ src/scroll.ts
@@ -1,4 +1,4 @@
-import { getElementWrapper, SElement } from "./misc";
+import { SElement } from "./misc";
 
 export type EasingFunction = (
 	currentStep: number,
@@ -37,129 +37,89 @@ function maxMin(value: number, max: number, min: number = 0) {
 	return Math.max(Math.min(value, max), min);
 }
 
-// const ScrollElements: {
-// 	element: HTMLElement | Window;
-// 	horizontal: boolean;
-// 	scrollAnimation: {
-// 		scrollAnimations: {
-// 			position: (time: number) => number;
-// 			duration: number;
-// 		}[];
-// 		virtualPosition: number;
-// 		finalPosition: number;
-// 		previousTime: number;
-// 	};
-// }[] = [];
+interface Animation {
+	initialTime: number;
+	duration: number;
+	easingFunction: EasingFunction;
+	distance: number;
+}
 
-// export interface IScroll {
-// 	container?: Element | Window | string;
-// 	horizontal?: boolean;
-// 	duration?: number;
-// 	easing?:
-// 		| string
-// 		| ((
-// 				currentStep: number,
-// 				offsetValue: number,
-// 				distance: number,
-// 				totalSteps: number
-// 		  ) => number);
-// 	onStop?: null | (() => void);
-// 	onScroll?: null | ((external?: boolean) => void);
-// }
-
-// function getElement({ horizontal, element }) {
-// 	let index = ScrollElements.findIndex(
-// 		(e) => element === e.element && horizontal === e.horizontal
-// 	);
-// 	if (index < 0) {
-// 		index = ScrollElements.length;
-// 		ScrollElements.push({
-// 			element,
-// 			horizontal,
-// 			scrollAnimation: {
-// 				scrollAnimations: [],
-// 				virtualPosition: 0,
-// 				finalPosition: 0,
-// 				previousTime: 0,
-// 			},
-// 		});
-// 	}
-// 	return ScrollElements[index].scrollAnimation;
-// }
-
-interface Animation {}
-
-class ScrollAnimation {
+class ScrollContainer {
+	constructor(
+		private horizontal: boolean = false,
+		options: {
+			private getScrollPosition;
+			private getScrollSize;
+			private scrollPosition;
+		}
+	);
 	animations: Animation[] = [];
 	virtualPosition: number = 0;
 	finalPosition: number = 0;
 	previousTime: number = 0;
+
+	private update(currentTime: number) {
+		const position = Math.round(this.getScrollPosition());
+		const diff =
+			position - Math.round(maxMin(this.virtualPosition, this.getScrollSize()));
+		this.finalPosition += diff;
+		onScroll && onScroll(!!diff);
+
+		this.animations = this.animations.filter(
+			({ initialTime, duration, easingFunction, distance }) => {
+				function getPosition(time: number) {
+					const currentDuration = maxMin(time - initialTime, duration, 0);
+					const offsetPosition = this.finalPosition - distance;
+					return easingFunction(
+						currentDuration,
+						offsetPosition,
+						distance,
+						duration
+					);
+				}
+				this.virtualPosition +=
+					getPosition(currentTime) - getPosition(this.previousTime);
+				return currentTime < duration;
+			}
+		);
+	}
+
+	scrollBy(distance, duration, easingFunction) {
+		const initialTime = performance.now();
+		scrollAnimation.finalPosition += distance;
+
+		const value = maxMin(
+			distance,
+			this.getScrollSize() - this.getScrollPosition(),
+			-this.getScrollPosition()
+		);
+
+		const animation: Animation = {
+			initialTime,
+			duration,
+			easingFunction,
+			distance: value,
+		};
+
+		this.animations.push(animation);
+		this.animations.length === 1 && this.update(initialTime);
+	}
 }
 
 export class ScrollUtility {
-	private myContainer = getElementWrapper(element, horizontal);
-	private verticalScrollAnimations = new ScrollAnimation();
-	private horizontalScrollAnimations = new ScrollAnimation();
+	private verticalScrollAnimations = new ScrollContainer();
+	private horizontalScrollAnimations = new ScrollContainer(true);
 	constructor(
-		private element,
+		private element = window,
 		private options: {
 			duration: number;
 			easing: EasingFunction;
 		} = {}
 	) {}
 
-	private update(currentTime: number) {
-		const position = Math.round(this.myContainer.scrollPosition());
-		const diff =
-			position -
-			Math.round(
-				maxMin(scrollAnimation.virtualPosition, this.myContainer.scrollSize())
-			);
-		scrollAnimation.finalPosition += diff;
-		onScroll && onScroll(!!diff);
-		scrollAnimation.scrollAnimations = scrollAnimation.scrollAnimations.filter(
-			(animation) => {
-				scrollAnimation.virtualPosition +=
-					animation.position(currentTime) -
-					animation.position(scrollAnimation.previousTime);
-				return currentTime < animation.duration;
-			}
-		);
-		this.myContainer.scrollBy(
-			Math.round(scrollAnimation.virtualPosition) - position
-		);
-		scrollAnimation.previousTime = currentTime;
-		!!scrollAnimation.scrollAnimations.length
-			? requestAnimationFrame(update)
-			: onStop && onStop();
-	}
-
-	private create(distance: number) {
-		const value = maxMin(
-			distance,
-			this.myContainer.scrollSize() - this.myContainer.scrollPosition(),
-			-this.myContainer.scrollPosition()
-		);
-		const initialTime = performance.now();
-		scrollAnimation.finalPosition += value;
-		const animation = {
-			initialTime,
-			duration,
-			getPosition: (time: number) => {
-				return easingFunction(
-					maxMin(time - initialTime, duration, 0),
-					scrollAnimation.finalPosition - value,
-					value,
-					duration
-				);
-			},
-		};
-		scrollAnimation.scrollAnimations.push(animation);
-		scrollAnimation.scrollAnimations.length === 1 && update(initialTime);
-	}
-
-	public stop() {
-		scrollAnimation.scrollAnimations = [];
+	stop() {
+		this.verticalScrollAnimations.stop();
+		this.horizontalScrollAnimations.stop();
 	}
 
 	get scrollX() {}
@@ -167,110 +127,11 @@ export class ScrollUtility {
 	set scrollX(x: number) {}
 	get scrollY(y: number) {}
 }
-
-export function optionalScroll(options: Required<IScroll>) {
-	const { container, horizontal, duration, easing, onStop, onScroll } = options;
-	const easingFunction = easingFromFunction(easing);
-	const element = getElementFromQuery(container);
-
-	function resetScrollAnimation() {
-		if (!scrollAnimation.scrollAnimations.length) {
-			(scrollAnimation.virtualPosition = myContainer.scrollPosition()),
-				(scrollAnimation.finalPosition = myContainer.scrollPosition()),
-				(scrollAnimation.previousTime = performance.now());
-		}
-	}
-
-	function create(distance: number) {
-		const value = maxMin(
-			distance,
-			myContainer.scrollSize() - myContainer.scrollPosition(),
-			-myContainer.scrollPosition()
-		);
-		const initialTime = performance.now();
-		scrollAnimation.finalPosition += value;
-		scrollAnimation.scrollAnimations.push({
-			duration: duration + initialTime,
-			position: (time: number) => {
-				return easingFunction(
-					maxMin(time - initialTime, duration, 0),
-					scrollAnimation.finalPosition - value,
-					value,
-					duration
-				);
-			},
-		});
-		scrollAnimation.scrollAnimations.length === 1 && update(initialTime);
-	}
-
-	function getRelativePosition(query: ElementOrQuery): number {
-		const elementRaw = getElementFromQuery(query);
-		if (elementRaw === myContainer.element)
-			return myContainer.scrollPosition() / myContainer.scrollSize();
-		const _element = getElementWrapper(elementRaw, myContainer.horizontal);
-		return getOffset(_element) / getDiff(_element);
-	}
-	function getDistToElement(query: ElementOrQuery, value = 0): number {
-		const element = getElementWrapper(
-			getElementFromQuery(query),
-			myContainer.horizontal
-		);
-		return getOffset(element) - getDiff(element) * value;
-	}
-	function getElementSize(query: ElementOrQuery, value = 1): number {
-		const element = getElementWrapper(
-			getElementFromQuery(query),
-			myContainer.horizontal
-		);
-		return element.size() * value;
-	}
-	function getOffset(el: SElement) {
-		return (
-			el.relativePosition() -
-			myContainer.relativePosition() -
-			myContainer.border()
-		);
-	}
-	function getDiff(el: SElement) {
-		return myContainer.size() - el.offset();
-	}
-	function scrollToValue(value: number) {
-		create(value - scrollAnimation.finalPosition);
-	}
-	function scrollToElement(query: ElementOrQuery, value: number) {
-		const element = getElementFromQuery(query);
-		const to =
-			element === myContainer.element
-				? myContainer.scrollSize() * value
-				: getDistToElement(element, value) + myContainer.scrollPosition();
-		scrollToValue(to);
-	}
-
-	return Object.assign({
-		getSize: myContainer.size,
-		getScrollSize: myContainer.scrollSize,
-		getScrollPosition: myContainer.scrollPosition,
-		getRelativePosition,
-		getDistToElement,
-		getElementSize,
-		stop,
-		scrollTo(...args: [number] | [ElementOrQuery, number]) {
-			resetScrollAnimation();
-			const _element: ElementOrQuery | null =
-				typeof args[0] === "number" ? null : args[0];
-			const value = typeof args[0] === "number" ? args[0] : args[1] || 0;
-			_element === null
-				? scrollToValue(value)
-				: scrollToElement(_element, value);
-		},
-		offset(...args: [number] | [ElementOrQuery, number]) {
-			resetScrollAnimation();
-			const _element: ElementOrQuery | null =
-				typeof args[0] === "number" ? null : args[0];
-			const value = typeof args[0] === "number" ? args[0] : args[1] || 0;
-			_element === null
-				? create(value)
-				: create(getElementSize(_element, value));
-		},
-	});
-}
+// function scrollToElement(query: ElementOrQuery, value: number) {
+// 	const element = getElementFromQuery(query);
+// 	const to =
+// 		element === myContainer.element
+// 		? myContainer.scrollSize() * value
+// 		: getDistToElement(element, value) + myContainer.scrollPosition();
+// 	scrollToValue(to);
+// }
