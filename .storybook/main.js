d473bfd8c3a5f2cfefc5bb6c58a6d610dc7ac53a
David Perez Alvarez <david@leddgroup.com>
2020-12-08 01:58:10 -0500

    wip storybook
    added cypress

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../docs/**/*.mdx",
+    "../docs/**/*.stories.mdx",
+    "../docs/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
